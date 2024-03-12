565244f0e25a4f68d66358a755f65bf6a64c41c9
Stephen Akinrodoye <stephenakinrodoye2@gmail.com>
2024-03-12 15:27:32 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const selectRating = document.querySelectorAll('ratings');
+for (i = 0; i < selectRating.length; i++) {
+    selectRating[i].style.color = 'hsl(217, 12%, 63%)';
+}
+console.log(selectRating);
